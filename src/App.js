d41b2af5d4a2ef56330a2791859086333c2e2204
azizbek <162705281+azizbek69@users.noreply.github.com>
2024-04-15 16:22:57 +0300

    Add files via upload
    page

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Fragment } from 'react';
+import "./App.css"
+
+function App() {
+  return (
+    <Fragment>
+      ok
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,39 @@
+import { Fragment } from "react"
+
+const Navigation = ({ className }) => {
+    className = "navbar navbar-expand-lg navbar-light bg-light " + className;
+    
+    return (
+        <Fragment>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Navbar scroll</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarScroll">
+                        <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </Fragment>
+    )
+}
+
+export default Navigation
